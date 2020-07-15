aaa293d651c510e7cb80f3e800a5901dd14c96c0
Nur Shalahuddin Fajri <shotozuro@gmail.com>
2020-07-15 23:20:46 +0700

    add visual regresion plugin

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -18,3 +18,16 @@ import './commands';
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
+import 'cypress-plugin-snapshots/commands';
+
+export const fixCypressSpec = (filename) => () => {
+  const path = require('path');
+  const relative = filename.substr(1); // removes leading "/"
+  const projectRoot = Cypress.config('projectRoot');
+  const absolute = path.join(projectRoot, relative);
+  Cypress.spec = {
+    absolute,
+    name: path.basename(filename),
+    relative,
+  };
+};
