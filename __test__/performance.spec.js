febe34a3e5d9a7c63c6d36fd37845cc787e2ef0d
Nur Shalahuddin Fajri <shotozuro@gmail.com>
2020-07-16 13:48:46 +0700

    fix error with done callback

--- __test__/performance.spec.js
+++ __test__/performance.spec.js
@@ -1,14 +1,15 @@
-const timeouts = [5000, 10000, 20000, 40000, 100000, 160000];
-// const timeout = 10000;
-timeouts.forEach((timeout) => {
-  describe('Performance Test', () => {
-    it(`test whether ${timeout}ms is enough to load the page`, (done) => {
-      cy.once('fail', (err) => {
+const timeouts = [1, 2, 5, 10, 20, 40, 100];
+describe('Performance Test', () => {
+  timeouts.forEach((timeout) => {
+    it(`test whether ${timeout} second is enough to load the page`, (done) => {
+      cy.on('fail', (err) => {
         expect(err.message).to.contains('Timed out after waiting');
         done();
         return false;
       });
-      cy.visit('/', { timeout });
+      cy.visit({ url: '/', timeout: timeout * 1000 }).then(() => {
+        done();
+      });
     });
   });
 });
